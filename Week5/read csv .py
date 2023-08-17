9d1b4f5e114df0f826447712ea70a015f3753db2
kavi patel <139360213+kaviptl@users.noreply.github.com>
2023-08-17 14:38:27 +0530

    Add files via upload

---
+++ Week5/read csv .py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+import csv
+
+
+# In[14]:
+
+
+with open ('C:\\sqlite3\\csv\\helloworld.txt','r',errors="ignore") as f:
+    c=f.read()
+    print(c)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
